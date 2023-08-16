9878d381809f01e586375a4c476dd36b00ca9fa4
amb <amber@space-iv.com>
2023-08-16 15:56:28 +0300

    basic setup of routing and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModuleOneComponent } from './module-one/module-one.component';
+import { HomeComponent } from './home/home.component';
+import { ModuleTwoComponent } from './module-two/module-two.component';
+import { ModuleThreeComponent } from './module-three/module-three.component';
 
-const routes: Routes = [];
+type PathMatch = 'full' | 'prefix' | undefined;
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full' as PathMatch, component: HomeComponent },
+  { path: 'one', component: ModuleOneComponent },
+  { path: 'two', component: ModuleTwoComponent },
+  { path: 'three', component: ModuleThreeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+
+import { ModuleOneComponent } from './module-one/module-one.component';
+import { HomeComponent } from './home/home.component';
+import { ModuleTwoComponent } from './module-two/module-two.component';
+import { ModuleThreeComponent } from './module-three/module-three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModuleOneComponent,
+    HomeComponent,
+    ModuleTwoComponent,
+    ModuleThreeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRippleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
